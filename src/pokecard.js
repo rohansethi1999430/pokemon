0d98eec57c806830df63f61d8cefa7dc04559911
Rohan <rohansethi1999430@icloud.com>
2021-05-09 15:50:12 +0530

    added props.name and pokemon image api

--- src/pokecard.js
+++ src/pokecard.js
@@ -1,11 +1,15 @@
 import React,{Component} from 'react'
+const poki_api='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
 
 class  pokecard extends Component {
     render(){
+        let imgSrc=`${poki_api}${this.props.id}.png`;
     return (
         <div className="Pokecard">
-            <h1>Welcome To Pokedex</h1>
+            
             <h1>{this.props.name}</h1>
+            <img src={imgSrc}/>
             
         </div>
     )
