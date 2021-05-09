7c95c21721ae1180291e725aebd8ab42ce47aacf
Rohan <rohansethi1999430@icloud.com>
2021-05-09 15:34:03 +0530

    created pokecard component

--- src/pokecard.js
+++ src/pokecard.js
@@ -4,7 +4,9 @@ class  pokecard extends Component {
     render(){
     return (
         <div className="Pokecard">
+            <h1>Welcome To Pokedex</h1>
             <h1>{this.props.name}</h1>
+            
         </div>
     )
 }
